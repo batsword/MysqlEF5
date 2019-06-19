c6550f4e187c48b9fccdbc00b13b946bec41f0d8
zyj2018 <14923349@163.com>
2019-06-19 11:27:16 +0800

    v1

---
+++ MysqlEF/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using EFCodeFirst;
+using MysqlEF.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MysqlEF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void insert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var user = new UserEntity()
+                {
+                    Name = "Test",
+                    Hobby = "ddd",
+                    Age = 29
+                };
+                using (var context = new DContext())
+                {
+                    context.Users.Add(user);
+                    context.SaveChanges();
+                    var accountUsers = context.Users.ToList();
+                }
+                Console.Write("{0}", user.Id);
+            }
+            catch (Exception ex)
+            {
+                Console.Write("{0}", ex);
+            }
+        }
+
+        private void read_Click(object sender, RoutedEventArgs e)
+        {
+            using (var context = new DContext())
+            {
+                var s = context.Users.Select(u => u);
+                foreach (var item in s)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ MysqlEF/codefirst/DContext.cs
@@ -0,0 +1,21 @@
+﻿using MysqlEF.models;
+using System.Data.Entity;
+
+namespace EFCodeFirst
+{
+    public class DContext : DbContext
+    {
+        /// <summary>
+        /// 添加构造函数,name为config文件中数据库连接字符串的name
+        /// </summary>
+        public DContext() : base("name=MyContext")
+        {
+
+        }
+
+        #region 数据集
+        public DbSet<UserEntity> Users { get; set; }
+
+        #endregion  
+    }
+}
